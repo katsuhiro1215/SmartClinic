325edde3be33ba7bf77ac052dc28ceccef1ecdfc
Katsuhiro Kakoi <katsuhiro.k1215@gmail.com>
2024-03-19 00:26:52 +0900

    Setting

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,49 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+Route::get('/inertia-test', function () {
+    return Inertia::render('InertiaTest');
+    }
+);
+
+Route::get('/component-test', function () {
+    return Inertia::render('ComponentTest');
+    }
+);
+
+Route::controller(InertiaTestController::class)->prefix('inertia')->group(function () {
+    Route::get('/', 'index')->name('inertia.index');
+    Route::get('create', 'create')->name('inertia.create');
+    Route::post('/', 'store')->name('inertia.store');
+    Route::get('{id}/show', 'show')->name('inertia.show');
+    Route::delete('{id}', 'delete')->name('inertia.delete');
+});
+
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
